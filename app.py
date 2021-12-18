3a873158be4ca459cfebddabf41fe07c645efa75
PudgyPigeon <mithmathmeth@gmail.com>
2021-12-18 18:26:01 +0800

    refactor: Puts UI element logic into separate functions

--- app.py
+++ app.py
@@ -1,40 +1,65 @@
 #Import library for h20wave
-from h2o_wave import site, ui
+from h2o_wave import ui, Q, app, main
 
-#Set up the base site
-page = site['/']
+#Main function which serves the site
+@app("/")
+async def serve(q):
+    
+    if not q.client.initialized:
+        new_user_setup(q)
+    
+    #Save page
+    await q.page.save()
+    
+    
+#Sets up site for new client/user
+def new_user_setup(q):
+        #Responsive layout
+    q.page["meta"] = ui.meta_card(
+        box="",
+        layouts=[
+            ui.layout(
+                breakpoint="xs",
+                zones=[
+                    ui.zone("header"),
+                    ui.zone("navigation"),
+                    ui.zone("content")
+                ]
+            )
+        ]
+    )
 
-#Responsive layout
-page["meta"] = ui.meta_card(
-    box="",
-    layouts=[
-        ui.layout(
-            breakpoint="xs",
-            zones=[
-                ui.zone("header"),
-                ui.zone("navigation"),
-                ui.zone("content")
-            ]
-        )
-    ]
-)
+    #Header
+    q.page["header"] = ui.header_card(
+        box="header",
+        title="Aggregated Visualizer",
+        subtitle="Allowing users to easily visualize large datasets"
+    )
+    #Navbar
+    q.page["navigation"] = ui.tab_card(
+        box="navigation",
+        items=[
+            ui.tab(name="table", label="Table View"),
+            ui.tab(name="plot", label="Plot View")
+        ]
+    )
+    
+    q.client.initialized = True
 
-#Header
-page["header"] = ui.header_card(
-    box="header",
-    title="Aggregated Visualizer",
-    subtitle="Allowing users to easily visualize large datasets"
-)
-
-#Navbar
-page["navigation"] = ui.tab_card(
-    box="navigation",
-    items=[
-        ui.tab(name="table", label="Table View"),
-        ui.tab(name="plot", label="Plot View")
-    ]
-)
-
-
-
-page.save()
+#Defines table view logic
+def table_view(q):
+    q.page["table_view"] = ui.form_card(
+        box="content",
+        items=[
+            ui.text_xl("Table view")
+        ]
+    )
+    
+#Defines plot view logic
+def plot_view(q):
+    q.page["table_view"] = ui.form_card(
+        box="content",
+        items=[
+            ui.text_xl("Plot view")
+        ]
+    )
